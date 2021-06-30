9b0c3f5acf7b30f1214e76bc092d9a545f4051e5
luismescobarf <luismescobarf@gmail.com>
2021-06-30 08:06:19 -0500

    Comentarios e imports

--- P61/Clase2/proyectoClase1/src/Ejercicio3_Jahir.java
+++ P61/Clase2/proyectoClase1/src/Ejercicio3_Jahir.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Ejercicio3_Jahir {
+    
+    // def potenciaDosYTres(numero1):
+    //     potencia2 = numero1*numero1
+    //     potencia3 = numero1*numero1*numero1
+    //     resultado = [potencia2,potencia3]
+    //     return resultado
+
+
     public static int[] potenciaDosYTres(int numero1)
      {
-          int[]resultado=new int[2];
+          int[]resultado = new int[2];
           int potencia2 = numero1*numero1;
           int potencia3 = numero1*numero1*numero1;
           resultado[0]=potencia2;
@@ -10,11 +20,14 @@ public class Ejercicio3_Jahir {
      }
 
      public static void main(String[] args) {
-          var sc = new Scanner(System.in);
-          System.out.println("Por favor ingrese un numero entero: ");
-          var numero1 = sc.nextInt();
-          var potencias = potenciaDosYTres(numero1);
-          System.out.println("El doble y triple del numero ingresado es: "+ potencias[0] +","+potencias[1]);
+
+        //int(input("Por favor ingrese un numero entero:"))
+        
+        var sc = new Scanner(System.in);
+        System.out.println("Por favor ingrese un numero entero: ");
+        var numero1 = sc.nextInt();
+        var potencias = potenciaDosYTres(numero1);
+        System.out.println("El doble y triple del numero ingresado es: "+ potencias[0] +","+potencias[1]);
           
      }
      
--- P61/Clase2/proyectoClase1/src/Ejercicio4_Freddie.java
+++ P61/Clase2/proyectoClase1/src/Ejercicio4_Freddie.java
@@ -1,6 +1,14 @@
 public class Ejercicio4_Freddie {
     
     public static void main(String[] args) {
+
+        //Si no tiene la palabra static
+        // Ejercicio3_Jahir instancia = new Ejercicio3_Jahir();
+        // instancia.potenciaDosYTres(numero1);
+
+        //Si tiene la plabra static
+        //Ejercicio3_Jahir.potenciaDosYTres(numero1);
+
         aviso();
         var numero = captura();
         System.out.println("El valor en grados Farenheit es :  "+pasaGrados(numero));
