95cce6ec6f5de94fe05af374b33a2d8d6265c826
luismescobarf <luismescobarf@gmail.com>
2021-06-30 09:11:12 -0500

    Primer proyecto MAVEN y requerimiento del número mágico

---
+++ P61/Clase2/proyecto_clase02/src/main/java/co/edu/utp/misiontic2022/c2/Ejercicio1.java
@@ -0,0 +1,14 @@
+package co.edu.utp.misiontic2022.c2;
+
+public class Ejercicio1 {
+
+    //Algoritmo número de la suerte
+    //1) Solicitar al usuario la fecha en formato String
+    //2) Extraer los pedazos que corresponden al día, al mes y al año (split)
+    //3) Cada pedazo convertirlo en un valor numérico
+    //4) Sumar los pedazos y almacenarlos
+    //5) De la sumatoria extraer las cifras
+    //6) Sumar las cifras y retornar el número de la suerte
+
+    
+}
