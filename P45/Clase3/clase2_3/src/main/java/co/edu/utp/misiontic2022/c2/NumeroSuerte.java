e321681c98940a8fa26894bd81c5a1aa7621affc
luismescobarf <luismescobarf@gmail.com>
2021-06-30 19:35:41 -0500

    Pasos numero suerte

---
+++ P45/Clase3/clase2_3/src/main/java/co/edu/utp/misiontic2022/c2/NumeroSuerte.java
@@ -0,0 +1,28 @@
+package co.edu.utp.misiontic2022.c2;
+
+public class NumeroSuerte {
+
+    //Algoritmo -> Paso a paso
+    //1) Obtener la fecha en formato String (Cadena DD/MM/AAAA)
+    //2) Separar los días, los meses, y el año de la fecha y los almacenamos
+    //3) Convertir días, meses y años a números
+    //4) Sumamos los días, los meses y los años y los almacenamos en una variable
+    //5) Separamos las cifras del número
+    //6) Sumamos las cifras del número del paso cinco
+    //7) Retornamos el número de la suerte que es el resultado de la suma
+    
+    //Declaración del método o función en Python
+    // def numeroSuerte(fechaNacimiento):
+    //     pass
+
+    //Función o método
+    public static int numeroSuerte(String fechaNacimiento){
+
+    }
+    
+    public static void main(String[] args) {
+        //Donde se va a ejecutar (sección principal)
+        //Equivalente en Python a lo que está pegado a la margen izquierda
+    }
+
+}
