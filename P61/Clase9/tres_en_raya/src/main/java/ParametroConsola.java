454af635707ea0bb79c690511fb59898c8f8c5f2
luismescobarf <luismescobarf@gmail.com>
2021-07-15 10:57:22 -0500

    Enumeraciones para controlar dominios de atributos y utilizar notación simbólica en la implementación

---
+++ P61/Clase9/tres_en_raya/src/main/java/ParametroConsola.java
@@ -0,0 +1,19 @@
+public enum ParametroConsola {
+
+    //Valores consola Jugadores
+    JUGADOR_O("O"), JUGADOR_X("X");
+
+    //Atributo que representa el valor
+    private String valorConsola;
+
+    //Constructor
+    ParametroConsola(String pValorConsola){
+        this.valorConsola = pValorConsola;
+    }
+
+    //Getter
+    public String getValorConsola() {
+        return valorConsola;
+    }
+    
+}
---
+++ P61/Clase9/tres_en_raya/src/main/java/co/edu/utp/misiontic2022/c2/ParametroLogico.java
@@ -0,0 +1,26 @@
+package co.edu.utp.misiontic2022.c2;
+
+public enum ParametroLogico {
+
+    //Valores lógicos JugadorO
+    JUGADOR_O(1), LINEA_JUGADOR_O(3),
+    //Valores lógicos JugadorX
+    JUGADOR_X(10), LINEA_JUGADOR_X(30),
+    //Situaciones de empate
+    EMPATE_INICIANDO_O(45), EMPATE_INICIANDO_X(54);
+
+    //Atributo que representa el valor
+    private int valorLogico;
+
+    //Constructor
+    ParametroLogico(int pValorLogico){
+        this.valorLogico = pValorLogico;
+    }
+
+    //Getter
+    public int getValorLogico() {
+        return valorLogico;
+    }
+
+    
+}
