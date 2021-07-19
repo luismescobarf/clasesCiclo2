95a543cb2dd3125b9004ba485af9b4b7f338d2f1
luismescobarf <luismescobarf@gmail.com>
2021-07-19 11:04:16 -0500

    Primera version del requerimiento reto1

---
+++ P61/Clase10/reto1_p45/src/main/java/mundoProblema/BecaUniversitaria.java
@@ -0,0 +1,71 @@
+package mundoProblema;
+
+public class BecaUniversitaria {
+   
+    //Atributos
+    private double monto;
+    private double interes;
+    private int tiempo;
+
+    //Constructores
+    public BecaUniversitaria(){
+        this.monto = 0;
+        this.interes = 0;
+        this.tiempo = 0;
+    }
+
+    public BecaUniversitaria(int pTiempo,double pMonto, double pInteres){
+        this.monto = pMonto;
+        this.interes = pInteres;
+        this.tiempo = pTiempo;
+    }
+
+    //Métodos
+    public double calcularInteresSimple(){
+        double interesSimple = this.monto * (this.interes/100) * this.tiempo;
+        return Math.round(interesSimple);
+    }    
+
+    public double calcularInteresCompuesto(){
+        double interesCompuesto = monto * (  Math.pow( 1+this.interes/100, this.tiempo) - 1 );
+        return Math.round(interesCompuesto);
+    }
+
+    public String compararInversion(){
+        double diferencia = 0;
+        //Calcular la diferencia
+        diferencia = this.calcularInteresCompuesto() - this.calcularInteresSimple();
+
+        if(diferencia == 0){
+            return "No se obtuvo diferencia entre las proyecciones, revisar los parámetros de entrada.";
+        }else{
+            return "La diferencia entre la proyección de interés compuesto e interés simple es: $" + diferencia;
+        }
+    }
+
+    public String compararInversion(int pTiempo,double pMonto, double pInteres){
+
+        //Cargamos en los atributos porque calcularInteresCompuesto y calcularInteresSimple trabajan sobre estos
+        this.monto = pMonto;
+        this.interes = pInteres;
+        this.tiempo = pTiempo;
+
+        return this.compararInversion();
+
+        // double diferencia = 0;
+        // //Calcular la diferencia
+        // diferencia = this.calcularInteresCompuesto() - this.calcularInteresSimple();
+
+        // if(diferencia == 0){
+        //     return "No se obtuvo diferencia entre las proyecciones, revisar los parámetros de entrada.";
+        // }else{
+        //     return "La diferencia entre la proyección de interés compuesto e interés simple es: $" + diferencia;
+        // }
+        
+    }
+
+
+    
+
+
+}
