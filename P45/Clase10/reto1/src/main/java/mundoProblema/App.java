b65ed12ca61f469578499692e548fe448e5f0ca5
luismescobarf <luismescobarf@gmail.com>
2021-07-19 20:36:34 -0500

    Reto1 diferente grupo P45

--- P45/Clase10/reto1/src/main/java/mundoProblema/App.java
+++ P45/Clase10/reto1/src/main/java/mundoProblema/App.java
@@ -8,6 +8,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //Caso de prueba 1
+        VentaProyecto ventaConstruccion = new VentaProyecto();
+        System.out.println(ventaConstruccion.calcularInteresSimple()); 
+        System.out.println(ventaConstruccion.calcularInteresCompuesto()); 
+        System.out.println(ventaConstruccion.compararInversion(36,200000,5.0));
+
+        //Caso de prueba 2
+        VentaProyecto ventaConstruccion2 = new VentaProyecto(12,150000,2.0);
+        System.out.println(ventaConstruccion2.calcularInteresSimple()); 
+        System.out.println(ventaConstruccion2.calcularInteresCompuesto()); 
+        System.out.println(ventaConstruccion2.compararInversion());
+
+        //Caso de prueba 3
+        VentaProyecto ventaConstruccion3 = new VentaProyecto();
+        System.out.println(ventaConstruccion3.calcularInteresSimple()); 
+        System.out.println(ventaConstruccion3.calcularInteresCompuesto()); 
+        System.out.println(ventaConstruccion3.compararInversion());
     }
 }
--- P45/Clase10/reto1/src/main/java/mundoProblema/VentaProyecto.java
+++ P45/Clase10/reto1/src/main/java/mundoProblema/VentaProyecto.java
@@ -57,31 +57,5 @@ public class VentaProyecto {
         return this.compararInversion();        
         
     }
-
-    //No subir el main a la plataforma -> Realizar pruebas locales antes de subir
-    public static void main(String[] args) {
-        
-        //Caso de prueba 1
-        VentaProyecto ventaConstruccion = new VentaProyecto();
-        System.out.println(ventaConstruccion.calcularInteresSimple()); 
-        System.out.println(ventaConstruccion.calcularInteresCompuesto()); 
-        System.out.println(ventaConstruccion.compararInversion(36,200000,5.0));
-
-        //Caso de prueba 2
-        VentaProyecto ventaConstruccion2 = new VentaProyecto(12,150000,2.0);
-        System.out.println(ventaConstruccion2.calcularInteresSimple()); 
-        System.out.println(ventaConstruccion2.calcularInteresCompuesto()); 
-        System.out.println(ventaConstruccion2.compararInversion());
-
-        //Caso de prueba 3
-        VentaProyecto ventaConstruccion3 = new VentaProyecto();
-        System.out.println(ventaConstruccion3.calcularInteresSimple()); 
-        System.out.println(ventaConstruccion3.calcularInteresCompuesto()); 
-        System.out.println(ventaConstruccion3.compararInversion());
-
-    }
-
-
-
     
 }
