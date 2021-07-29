163c9473baca1efa0738f919eb8899f4f0f1fc46
luismescobarf <luismescobarf@gmail.com>
2021-07-28 19:55:15 -0500

    Manipulación de cadenas nivel 2 con SQL -> DML

--- P45/Clase14/Consultas.sql
+++ P45/Clase14/Consultas.sql
@@ -40,5 +40,62 @@ LIMIT 10;
 
 -- ALTER TABLE Proyecto DROP COLUMN nuevaColumna;
 
+-- Requerimientos sobre Reto3 -> Conceptos de consultas SQL
+
+-- Generar los correos electrónicos de los líderes de los 
+-- proyectos con el siguiente formato, y arrojarlos
+-- ordenados por longitud de forma descendente. Nombrar
+-- la columna resultante como email_lider:
+-- 1) La primera letra del nombre en minúscula.
+-- 2) Un punto
+-- 3) Primer apellido
+-- 4) Colocar el ID_Lider
+-- 5) Dominio @empresa.com.co
+
+-- Extraer pedazos de cadena:
+SELECT  SUBSTR(Lider.Nombre, 1, 3) as Pedazo_Nombre, 
+        Lider.Nombre
+FROM Lider;
+
+SELECT  LOWER(SUBSTR(Lider.Nombre, 1, 1)) as Inicial, 
+        Lider.Nombre
+FROM Lider;
+
+-- Respuesta al requerimiento
+SELECT  LOWER(SUBSTR(Lider.Nombre, 1, 1)) || "." ||
+        Lider.Primer_Apellido || Lider.ID_Lider
+        || "@empresa.com.co" AS email_lider
+FROM Lider
+ORDER BY LENGTH(email_lider) DESC;
+
+-- Sin ordenar por longitud de correo generado
+SELECT  LOWER(SUBSTR(Lider.Nombre, 1, 1)) || "." ||
+        Lider.Primer_Apellido || Lider.ID_Lider
+        || "@empresa.com.co" AS email_lider
+FROM Lider;
+
+
+-- Revisión de la consulta
+SELECT  LOWER(SUBSTR(Lider.Nombre, 1, 1)) || "." ||
+        Lider.Primer_Apellido || Lider.ID_Lider
+        || "@empresa.com.co" AS email_lider,
+        Lider.Nombre,
+        Lider.Primer_Apellido,
+        Lider.ID_Lider
+FROM Lider
+ORDER BY LENGTH(email_lider) DESC;
+
+-- Variante sacando proción final del apellido
+SELECT  LOWER(SUBSTR(Lider.Nombre, 1, 1)) || "." ||
+        SUBSTR(Lider.Primer_Apellido, LENGTH(Lider.Primer_Apellido)-2, 3)        
+        || Lider.ID_Lider
+        || "@empresa.com.co" AS email_lider,
+        Lider.Nombre,
+        Lider.Primer_Apellido,
+        Lider.ID_Lider
+FROM Lider
+ORDER BY LENGTH(email_lider) DESC;
+
+
 
 
