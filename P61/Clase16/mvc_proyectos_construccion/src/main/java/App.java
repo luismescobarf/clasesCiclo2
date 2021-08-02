c350c13ea3d7f65289268f0f09db247acc639293
luismescobarf <luismescobarf@gmail.com>
2021-08-02 12:14:09 -0500

    Driver con Maven para SQLite3 y conexión exitosa de proyecto MVC

--- P61/Clase16/mvc_proyectos_construccion/src/main/java/App.java
+++ P61/Clase16/mvc_proyectos_construccion/src/main/java/App.java
@@ -1,13 +1,32 @@
-package model;
+import util.JDBCUtilities;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
- * Hello world!
+ * Aplicación Proyectos Construcción
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {               
+
+        //Intentar conectarnos a la base de datos
+        try(Connection conexion = JDBCUtilities.getConnection()){
+            
+            if(JDBCUtilities.estaVacia()){
+                System.out.println("Se encuentra vacía!");
+            }else{
+                System.out.println("No está vacía BD encontrada");
+            }
+
+            //Iniciaríamos nuestra aplicación
+            //Llamado al menú de inicio de la vista/view
+
+            
+        }catch( SQLException e ){
+
+            System.err.println("Error iniciando conexión: "+e);
+
+        }
+
     }
 }
