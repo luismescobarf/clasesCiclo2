39e9796e28ec866d96029bf9080c095739cf2b21
luismescobarf <luismescobarf@gmail.com>
2021-08-05 11:15:51 -0500

    Alternativas JOIN

--- P61/Clase17/solucionReto3/requerimientosReto3.sql
+++ P61/Clase17/solucionReto3/requerimientosReto3.sql
@@ -14,4 +14,33 @@ GROUP BY p.ID_Proyecto
 ORDER BY Gasto_Compras DESC
 LIMIT 10;
 
+--Mónica
+SELECT  c.ID_Proyecto, 
+        p.Clasificacion, 
+        SUM(mc.Precio_Unidad * c.Cantidad) as Gasto_Compras, 
+        p.Serial 
+FROM Compra c
+JOIN MaterialConstruccion mc ON
+c.ID_MaterialConstruccion = mc.ID_MaterialConstruccion
+JOIN Proyecto p ON
+c.ID_Proyecto = p.ID_Proyecto 
+GROUP BY c.ID_Proyecto 
+ORDER BY Gasto_Compras DESC
+LIMIT 10;
+
+--Mauricio
+SELECT  c.ID_Proyecto, 
+        p.Clasificacion, 
+        SUM(c.Cantidad * mc.Precio_Unidad) AS Gasto_Compras ,
+        p.Serial 
+FROM Compra c 
+JOIN MaterialConstruccion mc 
+ON mc.ID_MaterialConstruccion = c.ID_MaterialConstruccion  
+JOIN Proyecto p 
+ON p.ID_Proyecto = c.ID_Proyecto 
+GROUP BY c.ID_Proyecto 
+ORDER BY Gasto_Compras DESC 
+LIMIT 10;
+
+
 
