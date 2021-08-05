4ec0d6050ed88c563c7f815450b7c86315929284
luismescobarf <luismescobarf@gmail.com>
2021-08-04 20:18:52 -0500

    Parte de los requerimientos

--- P45/Clase17/solucionReto3/consultasRequerimientos.sql
+++ P45/Clase17/solucionReto3/consultasRequerimientos.sql
@@ -48,5 +48,40 @@ WHERE Lider.Ciudad_Residencia NOT LIKE "Bogota"
 GROUP BY Ciudad_Residencia 
 ORDER BY Numero_Lideres DESC;
 
+--Ejemplo uso del having:
+-- Solamente presentar las ciudades diferentes a Bogotá donde el 
+-- número de líderes oscila entre 2 y 5.
+SELECT  Ciudad_Residencia, 
+        COUNT(ID_Lider) as Numero_Lideres 
+FROM Lider 
+WHERE Lider.Ciudad_Residencia NOT LIKE "Bogota"
+GROUP BY Ciudad_Residencia
+HAVING Numero_Lideres BETWEEN 2 AND 5
+ORDER BY Numero_Lideres DESC;
+
+SELECT  Ciudad_Residencia, 
+        COUNT(ID_Lider) as Numero_Lideres 
+FROM Lider 
+WHERE  Lider.Ciudad_Residencia NOT LIKE "Bogota"        
+GROUP BY Ciudad_Residencia
+HAVING Numero_Lideres >=2 and Numero_Lideres <=5
+ORDER BY Numero_Lideres DESC;
+
+-- Requerimiento3: Los materiales de producción 
+-- nacional más comprados en los proyectos, 
+-- mostrando cuántas compras se han hecho de cada uno. 
+-- Desempatar alfabéticamente.
+
+SELECT  m.Nombre_Material,
+        m.Importado,
+        COUNT(*) as No_Compras
+FROM MaterialConstruccion as m
+INNER JOIN Compra c 
+ON m.ID_MaterialConstruccion = c.ID_MaterialConstruccion
+WHERE m.Importado = "No"
+GROUP BY m.ID_MaterialConstruccion
+ORDER BY No_Compras DESC,
+        m.Nombre_Material ASC;
+
 
 
