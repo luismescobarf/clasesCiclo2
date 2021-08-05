bc202992bf4f0ee368b1ce3aedaf74fd477c3ea6
luismescobarf <luismescobarf@gmail.com>
2021-08-05 10:09:54 -0500

    Inicio Clase 17 P61: Reto3 y Continuación MVC (Reto4)

---
+++ P61/Clase17/solucionReto3/requerimientosReto3.sql
@@ -0,0 +1,15 @@
+Temas Clase
+-----------
+
+- Enunciado e inquietudes Reto3 -> A punto de subirse pero está disponible en Telegram desde el sábado.
+- Conexión/Interacción a la base de datos desde SW (API JDBC) -> Patrón de diseño Modelo Vista Controlador (MVC)
+    - Se aplicará sobre caso del Reto3 -> Proyectos y Líderes.
+
+Enlaces de Interés:
+-------------------
+Repositorio de Maven:
+https://mvnrepository.com/
+SQLite para JDBC:
+https://mvnrepository.com/artifact/org.xerial/sqlite-jdbc
+
+
