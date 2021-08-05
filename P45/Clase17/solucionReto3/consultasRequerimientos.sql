21e8d52e5512cf4a9864f544af886ad75cec7efa
luismescobarf <luismescobarf@gmail.com>
2021-08-04 19:53:31 -0500

    Parte de los requerimientos

--- P45/Clase17/solucionReto3/consultasRequerimientos.sql
+++ P45/Clase17/solucionReto3/consultasRequerimientos.sql
@@ -1,15 +1,52 @@
-Temas Clase
------------
+-- Requerimiento1: tres meses en los que se realizan más compras
+SELECT  strftime("%m",Compra.Fecha) as Mes,
+        COUNT(*) Numero_Compras
+FROM Compra
+GROUP BY Mes
+ORDER BY Numero_Compras DESC
+LIMIT 3;
 
-- Enunciado e inquietudes Reto3 -> A punto de subirse pero está disponible en Telegram desde el sábado.
-- Conexión/Interacción a la base de datos desde SW (API JDBC) -> Patrón de diseño Modelo Vista Controlador (MVC)
-    - Se aplicará sobre caso del Reto3 -> Proyectos y Líderes.
+SELECT  strftime("%m",Compra.Fecha) as Mes,
+        COUNT(*) Numero_Compras
+FROM Compra
+GROUP BY Mes
+ORDER BY Numero_Compras DESC
+LIMIT 3;
 
-Enlaces de Interés:
--------------------
-Repositorio de Maven:
-https://mvnrepository.com/
-SQLite para JDBC:
-https://mvnrepository.com/artifact/org.xerial/sqlite-jdbc
+-- SELECT SUBSTR(Compra.Fecha, 6, 2) as Mes
+-- FROM Compra
+-- LIMIT 15;
+
+SELECT  SUBSTR(Compra.Fecha, 6, 2) as Mes,
+        COUNT(*) Numero_Compras
+FROM Compra
+GROUP BY Mes
+ORDER BY Numero_Compras DESC
+LIMIT 3;
+
+-- Requerimiento 2: Ciudad diferente a Bogotá
+-- donde residen más líderes para abrir sucursal
+SELECT  Lider.Ciudad_Residencia,
+        COUNT(*) Numero_Lideres
+FROM Lider
+WHERE Lider.Ciudad_Residencia NOT IN ('Bogota')
+GROUP BY Lider.Ciudad_Residencia
+ORDER BY Numero_Lideres DESC;
+-- LIMIT 1;
+
+SELECT  Ciudad_Residencia, 
+        COUNT(ID_Lider) as Numero_Lideres 
+FROM Lider 
+WHERE Ciudad_Residencia != "Bogota" 
+GROUP BY Ciudad_Residencia 
+ORDER BY Numero_Lideres DESC;
+
+SELECT  Ciudad_Residencia, 
+        COUNT(ID_Lider) as Numero_Lideres 
+FROM Lider 
+WHERE Lider.Ciudad_Residencia NOT LIKE "Bogota"
+GROUP BY Ciudad_Residencia 
+ORDER BY Numero_Lideres DESC;
+
 
 
